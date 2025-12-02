95b1f9442e75acc909963a5ebc4e73d9689a1500
bshen95 <bojie.shen@monash.edu>
2025-12-02 14:34:00 +1100

    add final version

---
+++ larger_training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Loop over seeds 1 to 25
+# for s in {1..25}; do
+# ./build/main -m scripts/map/random-32-32-20.map -t 1 -N 600 --objective 2 -s $s -v 3 --traffic 10 
+# ./build/main -m scripts/map/random-64-64-20.map -t 60 -N 1000 --objective 2 -s $s -v 3 --traffic 10
+# ./build/main -m scripts/map/empty-32-32.map -t 60 -N 600 --objective 2 -s $s -v 3 --traffic 10
+# ./build/main -m scripts/map/empty-48-48.map -t 60 -N 1000 --objective 2 -s $s -v 3 --traffic 10
+# ./build/main -m scripts/map/maze-32-32-4.map -t 60 -N 400 --objective 2 -s $s -v 3 --traffic 10
+# ./build/main -m scripts/map/room-64-64-8.map -t 60 -N 1000 --objective 2 -s $s -v 3 --traffic 10
+# ./build/main -m scripts/map/warehouse-10-20-10-2-2.map -t 60 -N 1000 --objective 2 -s $s -v 3 --traffic 10
+# ./build/main -m scripts/map/warehouse-10-20-10-2-1.map -t 60 -N 1000 --objective 2 -s $s -v 3 --traffic 10
+# done
